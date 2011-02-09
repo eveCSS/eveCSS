6ee48300dfb9dd8c1f2f84733d4d0447543490fc
mmichals <devnull@localhost>
2011-02-09 10:17:16 +0100

    code formatting & JavaDoc corrections/additions

--- src/de/ptb/epics/eve/data/measuringstation/exceptions/ParentNotAllowedException.java
+++ src/de/ptb/epics/eve/data/measuringstation/exceptions/ParentNotAllowedException.java
@@ -1,20 +1,20 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2001, 2007 Physikalisch Technische Bundesanstalt.
  * All rights reserved.
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ */
 package de.ptb.epics.eve.data.measuringstation.exceptions;
 
 /**
- * This Exception indicates, that a parent was set, that was not allowed in the context.
+ * This Exception indicates, that a parent was set, that was not allowed in the 
+ * context.
  * 
  * @author Stephan Rehfeld <stephan.rehfeld (- at -) ptb.de >
  *
  */
 public class ParentNotAllowedException extends Exception {
-	
 	
 	/** The id for serialization.
 	 * 
@@ -27,8 +27,7 @@ public class ParentNotAllowedException extends Exception {
 	 * 
 	 * @param message  The message that describes the problem.
 	 */
-	public ParentNotAllowedException( final String message ) {
-		super( message );
+	public ParentNotAllowedException(final String message) {
+		super(message);
 	}
-
 }
