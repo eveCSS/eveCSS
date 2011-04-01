4a1bde8d43a72f3c9d225011b525028f4fdb6c0e
mmichals <devnull@localhost>
2011-04-01 16:51:45 +0200

    addressed issues in Bug #144 (not resolved) - code formatting & JavaDoc - included file open filter "*.scml"

--- src/de/ptb/epics/eve/editor/wizards/NewScanDescriptionWizard.java
+++ src/de/ptb/epics/eve/editor/wizards/NewScanDescriptionWizard.java
@@ -7,9 +7,8 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.core.runtime.*;
 import org.eclipse.jface.operation.*;
 import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.CoreException;
 import java.io.*;
 
@@ -18,9 +17,6 @@ import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
-import org.eclipse.core.filesystem.provider.FileStore;
-
-import org.eclipse.debug.core.sourcelookup.containers.LocalFileStorage;
 
 import de.ptb.epics.eve.data.measuringstation.IMeasuringStation;
 import de.ptb.epics.eve.data.scandescription.Chain;
@@ -30,22 +26,18 @@ import de.ptb.epics.eve.data.scandescription.processors.ScanDescriptionSaverToXM
 import de.ptb.epics.eve.editor.Activator;
 
 /**
- * This is a sample new wizard. Its role is to create a new file 
- * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace 
- * when the wizard is opened, it will accept it as the target
- * container. The wizard creates one file with the extension
- * "scml". If a sample multi-page editor (also available
- * as a template) is registered for the same extension, it will
- * be able to open it.
+ * <code>NewScandescriptionWizard</code>.
+ * 
+ * @author ?
+ * @author Marcus Michalsky
  */
-
 public class NewScanDescriptionWizard extends Wizard implements INewWizard {
+	
+	// the only page in the wizard
 	private NewScanDescriptionWizardPage page;
-	private ISelection selection;
 
 	/**
-	 * Constructor for NewScanDescriptionWizard.
+	 * Constructs a <code>NewScanDescriptionWizard</code>.
 	 */
 	public NewScanDescriptionWizard() {
 		super();
@@ -53,26 +45,35 @@ public class NewScanDescriptionWizard extends Wizard implements INewWizard {
 	}
 	
 	/**
-	 * Adding the page to the wizard.
+	 * {@inheritDoc}
 	 */
-
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	public void addPages() {
-		page = new NewScanDescriptionWizardPage(selection);
+		page = new NewScanDescriptionWizardPage();
 		addPage(page);
 	}
 
 	/**
-	 * This method is called when 'Finish' button is pressed in
-	 * the wizard. We will create an operation and run it
-	 * using wizard as execution context.
+	 * {@inheritDoc}
 	 */
+	@Override
 	public boolean performFinish() {
 		
 		final String fileName = page.getFileName();
+		
 		IRunnableWithProgress op = new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+			@Override
+			public void run(IProgressMonitor monitor) 
+											throws InvocationTargetException {
 				try {
-					doFinish( fileName, monitor );
+					doFinish(fileName, monitor);
 				} catch (CoreException e) {
 					throw new InvocationTargetException(e);
 				} finally {
@@ -80,6 +81,7 @@ public class NewScanDescriptionWizard extends Wizard implements INewWizard {
 				}
 			}
 		};
+		
 		try {
 			getContainer().run(true, false, op);
 		} catch (InterruptedException e) {
@@ -89,24 +91,25 @@ public class NewScanDescriptionWizard extends Wizard implements INewWizard {
 			MessageDialog.openError(getShell(), "Error", realException.getMessage());
 			return false;
 		}
+		
 		return true;
 	}
 	
 	/**
 	 * The worker method. It will find the container, create the
 	 * file if missing or just replace its contents, and open
-	 * the editor on the newly created file.
+	 * the editor on the newly created file.<br><br>
 	 */
-
-	private void doFinish( String fileName, IProgressMonitor monitor ) throws CoreException {
+	private void doFinish(String fileName, IProgressMonitor monitor) 
+														throws CoreException {
 		// create a sample file
 		monitor.beginTask("Creating " + fileName, 2);
 		
 		monitor.worked(1);
 		monitor.setTaskName("Opening file for editing...");
 		
-		final File file = new File( fileName );
-		if( !file.exists() ) {
+		final File file = new File(fileName);
+		if(!file.exists()) {
 			try {
 				file.createNewFile();
 			} catch (IOException e) {
@@ -115,91 +118,47 @@ public class NewScanDescriptionWizard extends Wizard implements INewWizard {
 			}
 		}
 		
-		final IMeasuringStation measuringStation =  Activator.getDefault().getMeasuringStation();
-		final ScanDescription scanDescription = new ScanDescription( measuringStation );
-		final Chain chain = new Chain( 1 );
-		final StartEvent startEvent = new StartEvent( scanDescription.getEventById( "S0" ) , chain );
-		chain.setStartEvent( startEvent );
-		scanDescription.add( chain );
+		final IMeasuringStation measuringStation =  
+				Activator.getDefault().getMeasuringStation();
+		final ScanDescription scanDescription = 
+				new ScanDescription(measuringStation);
+		final Chain chain = new Chain(1);
+		final StartEvent startEvent = 
+				new StartEvent(scanDescription.getEventById("S0") , chain);
+		chain.setStartEvent(startEvent);
+		scanDescription.add(chain);
 		
 		try {
-			final FileOutputStream os = new FileOutputStream( file );
-			final ScanDescriptionSaverToXMLusingXerces scanDescriptionSaver = new ScanDescriptionSaverToXMLusingXerces( os, measuringStation, scanDescription );
+			final FileOutputStream os = new FileOutputStream(file);
+			final ScanDescriptionSaverToXMLusingXerces scanDescriptionSaver = 
+					new ScanDescriptionSaverToXMLusingXerces(os, 
+															 measuringStation, 
+															 scanDescription);
 			scanDescriptionSaver.save();
-		} catch( final FileNotFoundException e1 ) {
+		} catch(final FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
-		final IFileStore fileStore = EFS.getLocalFileSystem().getStore( new Path( fileName ) );
-		final FileStoreEditorInput fileStoreEditorInput = new FileStoreEditorInput( fileStore );
-		
+		final IFileStore fileStore = 
+				EFS.getLocalFileSystem().getStore(new Path(fileName));
+		final FileStoreEditorInput fileStoreEditorInput = 
+				new FileStoreEditorInput(fileStore);	
 			
 		getShell().getDisplay().asyncExec(new Runnable() {
 			public void run() {
-				IWorkbenchPage page =
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				IWorkbenchPage page = PlatformUI.getWorkbench().
+												 getActiveWorkbenchWindow().
+												 getActivePage();
 				try {
-					IDE.openEditor( page, fileStoreEditorInput, "de.ptb.epics.eve.editor.graphical.GraphicalEditor" );
-				} catch( final PartInitException e ) {
+					IDE.openEditor(page, fileStoreEditorInput, 
+						"de.ptb.epics.eve.editor.graphical.GraphicalEditor");
+				} catch(final PartInitException e) {
+					// TODO
 				}
 			}
 		});
 		
-		// IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		//IResource resource = root.findMember(new Path(containerName));
-		//if (!resource.exists() || !(resource instanceof IContainer)) {
-		//	throwCoreException("Container \"" + containerName + "\" does not exist.");
-		//}
-		//IContainer container = (IContainer) resource;
-		//final IFile file = container.getFile(new Path(fileName));
-		/*try {
-			InputStream stream = openContentStream();
-			if (file.exists()) {
-				file.setContents(stream, true, true, monitor);
-			} else {
-				file.create(stream, true, monitor);
-			}
-			stream.close();
-		} catch (IOException e) {
-		}
 		monitor.worked(1);
-		monitor.setTaskName("Opening file for editing...");
-		getShell().getDisplay().asyncExec(new Runnable() {
-			public void run() {
-				IWorkbenchPage page =
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-				try {
-					IDE.openEditor(page, file, true);
-				} catch (PartInitException e) {
-				}
-			}
-		});*/
-		monitor.worked(1);
-	}
-	
-	/**
-	 * We will initialize file contents with a sample text.
-	 */
-
-	private InputStream openContentStream() {
-		String contents =
-			"This is the initial file contents for *.scml file that should be word-sorted in the Preview page of the multi-page editor";
-		return new ByteArrayInputStream(contents.getBytes());
-	}
-
-	private void throwCoreException(String message) throws CoreException {
-		IStatus status =
-			new Status(IStatus.ERROR, "de.ptb.epics.eve.editor", IStatus.OK, message, null);
-		throw new CoreException(status);
-	}
-
-	/**
-	 * We will accept the selection in the workbench to see if
-	 * we can initialize from it.
-	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
-	 */
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		this.selection = selection;
 	}
 }
--- src/de/ptb/epics/eve/editor/wizards/NewScanDescriptionWizardPage.java
+++ src/de/ptb/epics/eve/editor/wizards/NewScanDescriptionWizardPage.java
@@ -2,18 +2,12 @@ package de.ptb.epics.eve.editor.wizards;
 
 import java.io.File;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -21,8 +15,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.dialogs.ContainerSelectionDialog;
-import org.eclipse.ui.dialogs.FileSelectionDialog;
 
 import de.ptb.epics.eve.editor.Activator;
 
@@ -36,24 +28,21 @@ public class NewScanDescriptionWizardPage extends WizardPage {
 	
 	private Text fileText;
 
-	//private ISelection selection;
-
 	/**
 	 * Constructor for SampleNewWizardPage.
-	 * 
-	 * @param pageName
+
 	 */
-	public NewScanDescriptionWizardPage( final ISelection selection ) {
+	public NewScanDescriptionWizardPage() {
 		super("wizardPage");
 		setTitle("New Scan Description");
 		setDescription("This wizard creates a new Scan Description.");
-		//this.selection = selection;
 	}
 
 	/**
-	 * @see IDialogPage#createControl(Composite)
+	 * {@inheritDoc}
 	 */
-	public void createControl( final Composite parent ) {
+	@Override
+	public void createControl(final Composite parent) {
 		
 		Composite container = new Composite(parent, SWT.NULL);
 		GridLayout layout = new GridLayout();
@@ -64,91 +53,109 @@ public class NewScanDescriptionWizardPage extends WizardPage {
 		Label label = new Label(container, SWT.NULL);
 		label.setText("&File name:");
 
-		fileText = new Text( container, SWT.BORDER | SWT.SINGLE );
-		GridData gd = new GridData( GridData.FILL_HORIZONTAL );
-		fileText.setLayoutData( gd );
-		fileText.addModifyListener( new ModifyListener() {
-			public void modifyText( final ModifyEvent e ) {
-				dialogChanged();
-			}
-		});
+		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		fileText.setLayoutData(gd);
+		fileText.addModifyListener(new FileTextModifiedListener());
 		
-		Button button = new Button( container, SWT.PUSH );
-		button.setText( "Browse..." );
-		button.addSelectionListener( new SelectionAdapter() {
-			public void widgetSelected( final SelectionEvent e ) {
-				handleBrowse();
-			}
-		});
+		Button button = new Button(container, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new buttonSelectionListener());
 		
-		initialize();
-		dialogChanged();
+		fileText.setText("");
 		setControl(container);
 	}
 
 	/**
-	 * Tests if the current workbench selection is a suitable container to use.
+	 * Returns the file name set by the user with browse...
+	 * 
+	 * @return the user selected file name
 	 */
-
-	private void initialize() {
-		fileText.setText( "" );
+	public String getFileName()
+	{
+		return fileText.getText();
 	}
-
+	
+	// ********************** Listeners *******************************
+	
 	/**
-	 * Uses the standard container selection dialog to choose the new value for
-	 * the container field.
+	 * <code>ModifyListener</code> of <code>fileText</code>.
 	 */
-
-	private void handleBrowse() {
-		//ContainerSelectionDialog dialog = new ContainerSelectionDialog(
-		//		getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
-		//		"Select new file container");
-
-		// als filePath wird das Messplatzverzeichnis gesetzt
-		int lastSeperatorIndex = Activator.getDefault().getMeasuringStation().getLoadedFileName().lastIndexOf( File.separatorChar );
-		final String filePath = Activator.getDefault().getMeasuringStation().getLoadedFileName().substring( 0, lastSeperatorIndex + 1 ) + "scan/";
-
-		final FileDialog dialog = new FileDialog( getShell(), SWT.SAVE );
-		dialog.setFilterPath(filePath);
-		final String fileName = dialog.open();
+	class FileTextModifiedListener implements ModifyListener {
 		
-		if( fileName != null ) {
-			this.fileText.setText( fileName );
-		}
-	}
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void modifyText(ModifyEvent e) {
+			
+			// read filename from text field
+			String fileName = fileText.getText();
 
-	/**
-	 * Ensures that both text fields are set.
-	 */
-
-	private void dialogChanged() {
-		String fileName = getFileName();
-
-		if (fileName.length() == 0) {
-			updateStatus("File name must be specified");
-			return;
-		}
-		//if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
-		//	updateStatus("File name must be valid");
-		//	return;
-		//}
-		int dotLoc = fileName.lastIndexOf('.');
-		if (dotLoc != -1) {
-			String ext = fileName.substring(dotLoc + 1);
-			if (ext.equalsIgnoreCase("scml") == false) {
-				updateStatus("File extension must be \"scml\"");
+			// show error message if no file name is given
+			if (fileName.length() == 0) {
+				setErrorMessage("File name must be specified!");
+				setPageComplete(false);
 				return;
 			}
+
+			// show error message if file extension is not scml
+			int dotLoc = fileName.lastIndexOf('.');
+		
+			if (dotLoc != -1) {
+				String ext = fileName.substring(dotLoc + 1);
+				if (!ext.equalsIgnoreCase("scml")) {
+					setErrorMessage("Extension must be \"scml\"");
+					setPageComplete(false);
+					return;
+				}
+			}
+			
+			// if none of the above events occurred everything is ok:
+			setErrorMessage(null);
+			setPageComplete(true);
+		}	
+	}
+	
+	/**
+	 * <code>SelectionListener</code> of <code>button</code>.
+	 */
+	class buttonSelectionListener implements SelectionListener {
+		
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {
 		}
-		updateStatus(null);
-	}
+		
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			
+			// file path = location of the measuring station description
+			int lastSeperatorIndex = Activator.getDefault().
+											   getMeasuringStation().
+											   getLoadedFileName().
+											   lastIndexOf(File.separatorChar);
+			final String filePath = Activator.getDefault().
+											  getMeasuringStation().
+											  getLoadedFileName().
+								substring(0, lastSeperatorIndex + 1) + "scan/";
 
-	private void updateStatus(String message) {
-		setErrorMessage(message);
-		setPageComplete(message == null);
-	}
-
-	public String getFileName() {
-		return fileText.getText();
+			// show a file dialog
+			final FileDialog dialog = new FileDialog(getShell(), SWT.SAVE);
+			dialog.setFilterPath(filePath);
+			String[] extensions = {"*.scml"};
+			dialog.setFilterExtensions(extensions);
+			final String fileName = dialog.open();
+			
+			// set selected file (if one was selected)
+			if(fileName != null) {
+				fileText.setText(fileName);
+			}
+		}
 	}
 }
