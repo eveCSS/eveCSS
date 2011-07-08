a305eaa4513e91f1731ca3cab0694a2776289170
mmichals <devnull@localhost>
2011-07-08 12:48:00 +0200

    changed color of MINOR to a darker yellow (the system color was too bright and therefore unreadable)

--- src/de/ptb/epics/eve/viewer/views/deviceinspectorview/DeviceInspectorView.java
+++ src/de/ptb/epics/eve/viewer/views/deviceinspectorview/DeviceInspectorView.java
@@ -30,6 +30,7 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IPartService;
