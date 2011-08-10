d931a30d74a16653a46f8dd91f3bca4f49ce005f
mmichals <devnull@localhost>
2011-08-10 15:41:01 +0200

    documentation for the newly created filter (performance issue)

--- src/de/ptb/epics/eve/data/measuringstation/filter/ExcludeDevicesOfScanModuleFilterManualUpdate.java
+++ src/de/ptb/epics/eve/data/measuringstation/filter/ExcludeDevicesOfScanModuleFilterManualUpdate.java
@@ -3,8 +3,15 @@ package de.ptb.epics.eve.data.measuringstation.filter;
 import de.ptb.epics.eve.data.scandescription.updatenotification.ModelUpdateEvent;
 
 /**
- * @author mmichals
- *
+ * <code>ExcludeDevicesOfScanModuleFilterManualUpdate</code> is a modified 
+ * version of 
+ * {@link de.ptb.epics.eve.data.measuringstation.filter.ExcludeDevicesOfScanModuleFilter}.
+ * It differs in the way it updates itself (when receiving update events). The 
+ * Update mechanism is suspended by default and can be activated (and 
+ * deactivated) via {@link #setUpdate(boolean)}. * 
+ * 
+ * @author Marcus Michalsky
+ * @since 0.4.2
  */
 public class ExcludeDevicesOfScanModuleFilterManualUpdate extends
 		ExcludeDevicesOfScanModuleFilter {
@@ -13,6 +20,7 @@ public class ExcludeDevicesOfScanModuleFilterManualUpdate extends
 	private boolean update;
 	
 	/**
+	 * Constructor.
 	 * 
 	 * @param excludeAxes
 	 * @param excludeChannels
@@ -29,7 +37,10 @@ public class ExcludeDevicesOfScanModuleFilterManualUpdate extends
 	}
 
 	/**
-	 * 
+	 * {@inheritDoc}
+	 * <br>
+	 * Only has the described effect if {@link #setUpdate(boolean)} is called 
+	 * with <code>true</code> as argument.
 	 */
 	@Override
 	public void updateEvent(ModelUpdateEvent modelUpdateEvent) {
@@ -39,9 +50,20 @@ public class ExcludeDevicesOfScanModuleFilterManualUpdate extends
 	}
 	
 	/**
-	 * 
+	 * Forces the filter to update itself.
 	 */
 	public void update() {
 		super.updateEvent(null);
+	}
+	
+	/**
+	 * Sets whether the filter should update itself when an update event is 
+	 * received.
+	 * 
+	 * @param update <code>true</code> if the filter should update itself when 
+	 * 		an update event is received, <code>false</code> otherwise
+	 */
+	public void setUpdate(boolean update) {
+		this.update = update;
 	}
 }
