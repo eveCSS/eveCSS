e4169e7cf0c0979e288d35a7eb67780a0e7d0d29
Marcus Michalsky <marcus.michalsky@ptb.de>
2012-03-23 17:53:18 +0100

    progress on feature #236: clear all axes/channels/...

---
+++ applications/plugins/org.csstudio.eve.editor/src/de/ptb/epics/eve/editor/handler/positioningcomposite/RemoveAllPositionings.java
@@ -0,0 +1,78 @@
+package de.ptb.epics.eve.editor.handler.positioningcomposite;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import de.ptb.epics.eve.data.scandescription.ScanModule;
+import de.ptb.epics.eve.editor.views.scanmoduleview.ScanModuleView;
+
+/**
+ * 
+ * 
+ * @author Marcus Michalsky
+ * @since 1.1
+ */
+public class RemoveAllPositionings implements IHandler {
+
+	private static Logger logger = 
+			Logger.getLogger(RemoveAllPositionings.class.getName());
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
+		if (activePart.getSite().getId().equals(
+				"de.ptb.epics.eve.editor.views.ScanModulView")) {
+			ScanModule sm = ((ScanModuleView)activePart).
+					getCurrentScanModule();
+			sm.removeAllPositionings();
+		} else {
+			logger.warn("Positionings could not be removed!");
+		}
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void dispose() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+	}
+}
