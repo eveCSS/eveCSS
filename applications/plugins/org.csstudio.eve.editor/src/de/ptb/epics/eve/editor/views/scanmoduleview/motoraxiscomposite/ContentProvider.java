9b6860806babfa31a3e4b2a9327b6113802ffe08
Marcus Michalsky <marcus.michalsky@ptb.de>
2012-04-13 16:31:10 +0200

    extracted superclass from composite content providers (ScanModuleView), implemented auto selection of elements after add/delete

---
+++ applications/plugins/org.csstudio.eve.editor/src/de/ptb/epics/eve/editor/views/scanmoduleview/motoraxiscomposite/ContentProvider.java
@@ -0,0 +1,20 @@
+package de.ptb.epics.eve.editor.views.scanmoduleview.motoraxiscomposite;
+
+import de.ptb.epics.eve.data.scandescription.ScanModule;
+import de.ptb.epics.eve.editor.views.scanmoduleview.CompositeContentProvider;
+
+/**
+ * @author Marcus Michalsky
+ * @since 1.2
+ * @see de.ptb.epics.eve.editor.views.scanmoduleview.CompositeContentProvider
+ */
+public class ContentProvider extends CompositeContentProvider {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Object[] getElements(Object inputElement) {
+		return ((ScanModule) inputElement).getAxes();
+	}
+}
