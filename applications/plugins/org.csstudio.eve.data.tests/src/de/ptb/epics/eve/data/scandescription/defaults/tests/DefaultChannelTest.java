274763846d4a841ddee20f831a86e3ea93675bf7
Marcus Michalsky <marcus.michalsky@ptb.de>
2012-12-13 18:13:19 +0100

    progress on feature #636: JUnit test for DefaultChannel initialization

---
+++ applications/plugins/org.csstudio.eve.data.tests/src/de/ptb/epics/eve/data/scandescription/defaults/tests/DefaultChannelTest.java
@@ -0,0 +1,107 @@
+package de.ptb.epics.eve.data.scandescription.defaults.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.ptb.epics.eve.data.scandescription.defaults.DefaultChannel;
+
+/**
+ * @author Marcus Michalsky
+ * @since 1.8
+ */
+public class DefaultChannelTest {
+
+	private DefaultChannel channel;
+	
+	/**
+	 * Tests whether a new instance of DefaultChannel has not set its average 
+	 * count.
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testAverageCountNotSet() {
+		this.channel.getAverageCount();
+	}
+	
+	/**
+	 * Tests whether a new instance of DefaultChannel has not set its max 
+	 * deviation.
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testMaxDeviationNotSet() {
+		this.channel.getMaxDeviation();
+	}
+	
+	/**
+	 * Tests whether a new instance of DefaultChannel has not set its minimum.
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testMinimumNotSet() {
+		this.channel.getMinimum();
+	}
+	
+	/**
+	 * Tests whether a new instance of DefaultChannel has not set its max 
+	 * attempts.
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testMaxAttemptsNotSet() {
+		this.channel.getMaxAttempts();
+	}
+	
+	/**
+	 * Tests whether a new instance of DefaultChannel has not set its normalize 
+	 * channel.
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testNormalizeChannelNotSet() {
+		this.channel.getNormalizeId();
+	}
+	
+	/**
+	 * Tests whether a new instance of DefaultChannel has initialized its 
+	 * deferred trigger with false.
+	 */
+	@Test
+	public void testDeferredTriggerSetToFalse() {
+		assertFalse(this.channel.isDeferred());
+	}
+	
+	// **********************************************************************
+	// **************************** Setup ***********************************
+	// **********************************************************************
+
+	/**
+	 * Initializes logging and loads the measuring station (Class wide setup
+	 * method of the test).
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() {
+	}
+
+	/**
+	 * Class wide tear down method.
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() {
+	}
+
+	/**
+	 * Test wide set up method.
+	 */
+	@Before
+	public void setUp() {
+		this.channel = new DefaultChannel();
+	}
+
+	/**
+	 * Test wide tear down method.
+	 */
+	@After
+	public void tearDown() {
+	}
+}
