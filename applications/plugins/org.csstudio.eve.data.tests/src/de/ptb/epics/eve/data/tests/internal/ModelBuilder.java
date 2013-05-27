03ce0fd21ee0aca2799e3357d4dde6a199b9036a
Marcus Michalsky <marcus.michalsky@ptb.de>
2013-05-27 16:12:24 +0200

    JUnit (#812)

--- applications/plugins/org.csstudio.eve.data.tests/src/de/ptb/epics/eve/data/tests/internal/ModelBuilder.java
+++ applications/plugins/org.csstudio.eve.data.tests/src/de/ptb/epics/eve/data/tests/internal/ModelBuilder.java
@@ -6,30 +6,30 @@ import de.ptb.epics.eve.data.measuringstation.Option;
 
 public class ModelBuilder {
 
-	protected static Motor createMotorWithAxisAndOptions() {
+	public static Motor createMotorWithAxisAndOptions() {
 		Motor m = ModelBuilder.createMotor("Motor1");
-		m.add(ModelBuilder.createOption("Motor1-Option1"));
-		MotorAxis ma = ModelBuilder.createMotorAxis("Motor1-Axis1");
+		// m.add(ModelBuilder.createOption("Motor1-Option1"));
+		//MotorAxis ma = ModelBuilder.createMotorAxis("Motor1-Axis1");
 		// ma.add(ModelBuilder.createOption("Motor1-Axis1-Option1"));
-		m.add(ma);
+		//m.add(ma);
 		return m;
 	}
 	
-	protected static Motor createMotor(String id) {
+	public static Motor createMotor(String id) {
 		Motor m = new Motor();
 		m.setId(id);
 		
 		return m;
 	}
 	
-	protected static MotorAxis createMotorAxis(String id) {
+	public static MotorAxis createMotorAxis(String id) {
 		MotorAxis ma = new MotorAxis();
 		ma.setId(id);
 		
 		return ma;
 	}
 	
-	protected static Option createOption(String id) {
+	public static Option createOption(String id) {
 		Option o = new Option();
 		o.setId(id);
 		
