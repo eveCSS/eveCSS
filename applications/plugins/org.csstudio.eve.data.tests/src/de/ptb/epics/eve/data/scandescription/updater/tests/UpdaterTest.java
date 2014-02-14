0d6dfcc2b0d1cbebb3f55167ba7ca857bb028148
Marcus Michalsky <marcus.michalsky@ptb.de>
2014-02-14 19:16:21 +0100

    ClassLoader System Resource path changes

--- applications/plugins/org.csstudio.eve.data.tests/src/de/ptb/epics/eve/data/scandescription/updater/tests/UpdaterTest.java
+++ applications/plugins/org.csstudio.eve.data.tests/src/de/ptb/epics/eve/data/scandescription/updater/tests/UpdaterTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -28,39 +27,39 @@ import de.ptb.epics.eve.util.data.Version;
  * @since 1.18
  */
 public class UpdaterTest {
-	
+
 	/**
-	 * Tests whether all included Updates are applied successfully by validating 
+	 * Tests whether all included Updates are applied successfully by validating
 	 * the result with the XML schema.
 	 */
 	@Test
 	public void testUpdate() {
 		try {
-			URL url = ClassLoader.getSystemResource("test.scml");
-			File testFile = new File(url.getPath());
+			File testFile = new File(ClassLoader.getSystemResource("test.scml")
+					.getFile());
 
-			url = ClassLoader.getSystemResource("schema.xsd");
 			DocumentBuilderFactory factory = DocumentBuilderFactory
 					.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document = builder.parse(new File(url.getPath()));
+			Document document = builder.parse(new File(ClassLoader
+					.getSystemResource("schema.xsd").getFile()));
 			Node node = document.getElementsByTagName("schema").item(0);
 			String versionString = node.getAttributes().getNamedItem("version")
 					.getNodeValue();
 			Version currentVersion = new Version(Integer.parseInt(versionString
 					.split("\\.")[0]), Integer.parseInt(versionString
 					.split("\\.")[1]));
-			
+
 			builder = factory.newDocumentBuilder();
 			document = builder.parse(testFile);
-			
+
 			Updater.getInstance().update(document, currentVersion);
-			
+
 			SchemaFactory schemaFactory = SchemaFactory
 					.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-			Schema schema = schemaFactory
-					.newSchema(url);
-			
+			Schema schema = schemaFactory.newSchema(ClassLoader
+					.getSystemResource("schema.xsd"));
+
 			schema.newValidator().validate(new DOMSource(document));
 		} catch (ParserConfigurationException e) {
 			fail(e.getMessage());
