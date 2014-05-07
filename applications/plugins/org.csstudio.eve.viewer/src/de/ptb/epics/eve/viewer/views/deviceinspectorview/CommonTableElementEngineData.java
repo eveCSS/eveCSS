840ced311dfef1206a237646d278a5cce2a2c90a
Marcus Michalsky <marcus.michalsky@ptb.de>
2014-05-07 15:22:40 +0200

    fixed bug #1395: sec timer values from engine

--- applications/plugins/org.csstudio.eve.viewer/src/de/ptb/epics/eve/viewer/views/deviceinspectorview/CommonTableElementEngineData.java
+++ applications/plugins/org.csstudio.eve.viewer/src/de/ptb/epics/eve/viewer/views/deviceinspectorview/CommonTableElementEngineData.java
@@ -22,7 +22,6 @@ public class CommonTableElementEngineData implements IMeasurementDataListener {
 	private Boolean isActive;
 	private String textvalue;
 	private CommonTableElement tableElement;
-	private Formatter formatter;
 	
 	/**
 	 * Constructs a <code>CommonTableElementEngineData</code<.
@@ -35,8 +34,6 @@ public class CommonTableElementEngineData implements IMeasurementDataListener {
 	public CommonTableElementEngineData(String dataId, CommonTableElement tableElement) {
 		this.dataId = dataId;
 		this.tableElement = tableElement;
-		this.formatter = new Formatter(
-				new Locale(Locale.ENGLISH.getCountry()));
 		Activator.getDefault().getEcp1Client().addMeasurementDataListener(this);
 		datamodifier = DataModifier.UNMODIFIED;
 		isActive = true;
@@ -49,7 +46,6 @@ public class CommonTableElementEngineData implements IMeasurementDataListener {
 	protected void finalize() throws Throwable {
 		Activator.getDefault().getEcp1Client()
 				.removeMeasurementDataListener(this);
-		this.formatter.close();
 		super.finalize();
 	}
 	
@@ -67,8 +63,11 @@ public class CommonTableElementEngineData implements IMeasurementDataListener {
 			if ((measurementData.getDataType() == DataType.DOUBLE) || 
 				(measurementData.getDataType() == DataType.FLOAT)) {
 					Double value = (Double) measurementData.getValues().get(0);
+					Formatter formatter = new Formatter(
+							new Locale(Locale.ENGLISH.getCountry()));
 					textvalue = formatter.format("%12.4g", value).out().
 							toString();
+					formatter.close();
 			} else {
 				textvalue = measurementData.getValues().get( 0 ).toString();
 			}
