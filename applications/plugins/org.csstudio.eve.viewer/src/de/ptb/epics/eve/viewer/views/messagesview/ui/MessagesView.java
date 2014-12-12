3c230637d4ea783199abfff67f569bde6547aeac
Marcus Michalsky <marcus.michalsky@ptb.de>
2014-12-12 14:26:42 +0100

    fixed bug #1750: wrong ordering of messages in  message view

--- applications/plugins/org.csstudio.eve.viewer/src/de/ptb/epics/eve/viewer/views/messagesview/ui/MessagesView.java
+++ applications/plugins/org.csstudio.eve.viewer/src/de/ptb/epics/eve/viewer/views/messagesview/ui/MessagesView.java
@@ -9,15 +9,9 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.FontMetrics;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IMemento;
-import org.eclipse.ui.IViewSite;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.ViewPart;
 
 import de.ptb.epics.eve.viewer.Activator;
@@ -26,7 +20,7 @@ import de.ptb.epics.eve.viewer.views.messagesview.LevelFilter;
 import de.ptb.epics.eve.viewer.views.messagesview.MessageFilter;
 import de.ptb.epics.eve.viewer.views.messagesview.MessageList;
 import de.ptb.epics.eve.viewer.views.messagesview.SourceFilter;
-import de.ptb.epics.eve.viewer.views.messagesview.TypeViewerComparator;
+import de.ptb.epics.eve.viewer.views.messagesview.TimeViewerComparator;
 
 /**
  * 
@@ -40,24 +34,9 @@ public final class MessagesView extends ViewPart implements PropertyChangeListen
 	// the only element in this view is a table viewer for the messages
 	private TableViewer tableViewer;
 	
-	private boolean sort;
-	private TypeViewerComparator typeViewerComparator;
 	private FilterSettings filterSettings;
 	
-	private Image sortImage;
-	
-	private IMemento memento;
-	
 	private int charWidth;
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void init(IViewSite site, IMemento memento) throws PartInitException {
-		this.memento = memento;
-		super.init(site, memento);
-	}
 	
 	/**
 	 * {@inheritDoc}
@@ -84,8 +63,8 @@ public final class MessagesView extends ViewPart implements PropertyChangeListen
 				SWT.LEFT);
 		typeColumn.getColumn().setText("");
 		typeColumn.getColumn().setWidth(25);
-		typeColumn.getColumn().addSelectionListener(
-				new TypeColumnSelectionListener());
+		//typeColumn.getColumn().addSelectionListener(
+		//		new TypeColumnSelectionListener());
 		
 		// fourth column: the text (contents) of the message
 		TableViewerColumn messageColumn = new TableViewerColumn(
@@ -107,18 +86,13 @@ public final class MessagesView extends ViewPart implements PropertyChangeListen
 		Activator.getDefault().getMessageList().addPropertyChangeListener(
 				MessageList.MESSAGE_MAX_WIDTH_PROP, this);
 		
-		this.sortImage = Activator.getDefault().getImageRegistry()
-				.get("SORTARROW");
-		this.sort = false;
-		this.typeViewerComparator = new TypeViewerComparator();
-		
 		this.filterSettings = new FilterSettings();
 		MessageFilter sourceFilter = new SourceFilter(this.filterSettings);
 		MessageFilter levelFilter = new LevelFilter(this.filterSettings);
 		this.tableViewer.setFilters(new ViewerFilter[] { sourceFilter,
 				levelFilter });
 		
-		this.restoreState();
+		this.tableViewer.setComparator(new TimeViewerComparator());
 		
 		GC gc = new GC(tableViewer.getTable());
 		FontMetrics fm = gc.getFontMetrics();
@@ -137,32 +111,6 @@ public final class MessagesView extends ViewPart implements PropertyChangeListen
 	 */
 	public void refresh() {
 		this.tableViewer.refresh();
-	}
-	
-	/*
-	 * 
-	 */
-	private void restoreState() {
-		if (this.memento == null) {
-			return;
-		}
-		this.sort = this.memento.getBoolean("sort") != null 
-					? this.memento.getBoolean("sort")
-					: false;
-
-		if (this.sort) {
-			tableViewer.getTable().getColumn(2).setImage(sortImage);
-			tableViewer.setComparator(typeViewerComparator);
-		}
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void saveState(IMemento memento) {
-		memento.putBoolean("sort", this.sort);
-		super.saveState(memento);
 	}
 	
 	/**
@@ -195,45 +143,6 @@ public final class MessagesView extends ViewPart implements PropertyChangeListen
 			this.tableViewer.getTable().getColumn(3)
 					.setWidth(((int) e.getNewValue()) * this.charWidth + 8);
 			//this.tableViewer.getTable().getColumn(3).pack();
-		}
-	}
-	
-	/* ******************************************************************** */
-	/* *************************** Listeners ****************************** */
-	/* ******************************************************************** */
-
-	/**
-	 * {@link org.eclipse.swt.events.SelectionListener} of 
-	 * typeColumn.
-	 * 
-	 * @author Marcus Michalsky
-	 * @since 1.4
-	 */
-	private class TypeColumnSelectionListener implements SelectionListener {
-
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public void widgetSelected(SelectionEvent e) {
-			if (sort) {
-				tableViewer.getTable().getColumn(2).setImage(null);
-				tableViewer.setComparator(null);
-				sort = false;
-			} else {
-				tableViewer.getTable().getColumn(2).setImage(sortImage);
-				tableViewer.setComparator(typeViewerComparator);
-				tableViewer.getTable().showItem(
-						tableViewer.getTable().getItem(0));
-				sort = true;
-			}
-		}
-
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public void widgetDefaultSelected(SelectionEvent e) {
 		}
 	}
 }
