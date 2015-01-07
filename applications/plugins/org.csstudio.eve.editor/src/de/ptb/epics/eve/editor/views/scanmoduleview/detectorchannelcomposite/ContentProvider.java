3e89c5577f6aef9924d3d7f55fbce1b76e4bd1c3
Marcus Michalsky <marcus.michalsky@ptb.de>
2015-01-07 13:11:34 +0100

    fixed bug #1769: wrong selection behavior in sorted table viewer

--- applications/plugins/org.csstudio.eve.editor/src/de/ptb/epics/eve/editor/views/scanmoduleview/detectorchannelcomposite/ContentProvider.java
+++ applications/plugins/org.csstudio.eve.editor/src/de/ptb/epics/eve/editor/views/scanmoduleview/detectorchannelcomposite/ContentProvider.java
@@ -1,20 +1,71 @@
 package de.ptb.epics.eve.editor.views.scanmoduleview.detectorchannelcomposite;
 
+import javafx.collections.ListChangeListener;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+
+import de.ptb.epics.eve.data.scandescription.Channel;
 import de.ptb.epics.eve.data.scandescription.ScanModule;
-import de.ptb.epics.eve.editor.views.scanmoduleview.CompositeContentProvider;
 
 /**
  * @author Marcus Michalsky
- * @since 1.2
- * @see de.ptb.epics.eve.editor.views.scanmoduleview.CompositeContentProvider
+ * @since 1.22
  */
-public class ContentProvider extends CompositeContentProvider {
-
+public class ContentProvider implements IStructuredContentProvider,
+		ListChangeListener<Channel> {
+	private static final Logger LOGGER = Logger.getLogger(ContentProvider.class
+			.getName());
+	private TableViewer viewer;
+	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public Object[] getElements(Object inputElement) {
 		return ((ScanModule)inputElement).getChannels();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		this.viewer = (TableViewer)viewer;
+		if (oldInput != null) {
+			((ScanModule)oldInput).removeChannelChangeListener(this);
+		}
+		if (newInput != null) {
+			((ScanModule)newInput).addChannelChangeListener(this);
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onChanged(
+			javafx.collections.ListChangeListener.Change<? extends Channel> change) {
+		this.viewer.refresh();
+		while (change.next()) {
+			if (change.wasAdded()) {
+				for (Channel channel : change.getAddedSubList()) {
+					LOGGER.debug("set selection to: " + 
+							channel.getDetectorChannel().getName());
+					this.viewer.setSelection(
+							new StructuredSelection(channel), true);
+				}
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void dispose() {
 	}
 }
--- applications/plugins/org.csstudio.eve.editor/src/de/ptb/epics/eve/editor/views/scanmoduleview/motoraxiscomposite/ContentProvider.java
+++ applications/plugins/org.csstudio.eve.editor/src/de/ptb/epics/eve/editor/views/scanmoduleview/motoraxiscomposite/ContentProvider.java
@@ -1,20 +1,71 @@
 package de.ptb.epics.eve.editor.views.scanmoduleview.motoraxiscomposite;
 
+import javafx.collections.ListChangeListener;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+
+import de.ptb.epics.eve.data.scandescription.Axis;
 import de.ptb.epics.eve.data.scandescription.ScanModule;
-import de.ptb.epics.eve.editor.views.scanmoduleview.CompositeContentProvider;
 
 /**
  * @author Marcus Michalsky
- * @since 1.2
- * @see de.ptb.epics.eve.editor.views.scanmoduleview.CompositeContentProvider
+ * @since 1.22
  */
-public class ContentProvider extends CompositeContentProvider {
-
+public class ContentProvider implements IStructuredContentProvider,
+		ListChangeListener<Axis> {
+	private static final Logger LOGGER = Logger.getLogger(ContentProvider.class
+			.getName());
+	private TableViewer viewer;
+	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public Object[] getElements(Object inputElement) {
 		return ((ScanModule) inputElement).getAxes();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		this.viewer = (TableViewer)viewer;
+		if (oldInput != null) {
+			((ScanModule)oldInput).removeAxisChangeListener(this);
+		}
+		if (newInput != null) {
+			((ScanModule)newInput).addAxisChangeListener(this);
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onChanged(
+			javafx.collections.ListChangeListener.Change<? extends Axis> change) {
+		this.viewer.refresh();
+		while (change.next()) {
+			if (change.wasAdded()) {
+				for (Axis axis : change.getAddedSubList()) {
+					LOGGER.debug("set selection to: "
+							+ axis.getMotorAxis().getName());
+					this.viewer.setSelection(new StructuredSelection(axis),
+							true);
+				}
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void dispose() {
 	}
 }
