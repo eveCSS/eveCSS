a7c11cff04c024fd84ebb37395430e83444fed9b
Marcus Michalsky <marcus.michalsky@ptb.de>
2015-06-11 19:12:24 +0200

    JUnit MainAxis Set and Remove MainAxis Tests

--- applications/plugins/org.csstudio.eve.data.tests/src/de/ptb/epics/eve/data/scandescription/tests/ScanModuleTest.java
+++ applications/plugins/org.csstudio.eve.data.tests/src/de/ptb/epics/eve/data/scandescription/tests/ScanModuleTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,22 +20,15 @@ import de.ptb.epics.eve.data.scandescription.Axis;
 import de.ptb.epics.eve.data.scandescription.ScanModule;
 import de.ptb.epics.eve.data.scandescription.Stepfunctions;
 
-public class ScanModuleTest {
+public class ScanModuleTest implements PropertyChangeListener {
 	private ScanModule scanModule;
+	private Axis axis1;
+	private Axis axis2;
+	private boolean mainAxis;
 	
 	@Test
-	public void testMainAxis() {
-		Axis axis1 = new Axis(this.scanModule);
-		Axis axis2 = new Axis(this.scanModule);
-		
-		MotorAxis mockAxis = mock(MotorAxis.class);
-		when(mockAxis.getType()).thenReturn(DataTypes.DOUBLE);
-		when(mockAxis.getDefaultValue()).thenReturn("0");
-		Function mockFunction = mock(Function.class);
-		when(mockFunction.isDiscrete()).thenReturn(false);
-		when(mockAxis.getGoto()).thenReturn(mockFunction);
-		axis1.setMotorAxis(mockAxis);
-		axis2.setMotorAxis(mockAxis);
+	public void testSetMainAxis() {
+		this.mockAxes();
 		
 		axis1.setStepfunction(Stepfunctions.ADD);
 		axis2.setStepfunction(Stepfunctions.ADD);
@@ -43,7 +39,7 @@ public class ScanModuleTest {
 		
 		axis2.setStart(0.0);
 		axis2.setStop(20.0);
-		axis2.setStepwidth(1.0);
+		axis2.setStepwidth(1.0); // should be 2 after setting main axis
 		
 		this.scanModule.add(axis1);
 		this.scanModule.add(axis2);
@@ -51,6 +47,63 @@ public class ScanModuleTest {
 		axis1.setMainAxis(true);
 		
 		assertEquals(2.0, (double) axis2.getStepwidth(), 0);
+	}
+	
+	@Test
+	public void testRemoveMainAxis() {
+		this.mockAxes();
+		
+		axis1.setStart(0.0);
+		axis1.setStop(100.0);
+		axis1.setStepwidth(10.0);
+		
+		axis2.setStart(0.0);
+		axis2.setStop(20.0);
+		axis2.setStepwidth(1.0); 
+		
+		this.scanModule.add(axis1);
+		this.scanModule.add(axis2);
+		
+		this.mainAxis = false;
+		
+		assertNull(this.scanModule.getMainAxis());
+		assertFalse(this.mainAxis);
+		
+		this.scanModule.addPropertyChangeListener(ScanModule.MAIN_AXIS_PROP, this);
+		axis1.setMainAxis(true);
+		assertTrue(this.mainAxis);
+		assertEquals(axis1, this.scanModule.getMainAxis());
+		
+		this.scanModule.remove(axis1);
+		
+		assertFalse(this.mainAxis);
+		assertNull(this.scanModule.getMainAxis());
+	}
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		switch (evt.getPropertyName()) {
+		case ScanModule.MAIN_AXIS_PROP:
+			if (evt.getNewValue() != null) {
+				this.mainAxis = true;
+			} else {
+				this.mainAxis = false;
+			}
+			break;
+		}
+	}
+	
+	private void mockAxes() {
+		axis1 = new Axis(this.scanModule);
+		axis2 = new Axis(this.scanModule);
+		MotorAxis mockAxis = mock(MotorAxis.class);
+		when(mockAxis.getType()).thenReturn(DataTypes.DOUBLE);
+		when(mockAxis.getDefaultValue()).thenReturn("0");
+		Function mockFunction = mock(Function.class);
+		when(mockFunction.isDiscrete()).thenReturn(false);
+		when(mockAxis.getGoto()).thenReturn(mockFunction);
+		axis1.setMotorAxis(mockAxis);
+		axis2.setMotorAxis(mockAxis);
 	}
 	
 	// ***********************************************************************
