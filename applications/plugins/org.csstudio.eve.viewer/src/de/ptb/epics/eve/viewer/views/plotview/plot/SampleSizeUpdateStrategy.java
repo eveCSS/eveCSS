97d005db023202edff0fe4def407ccaa2f867077
Marcus Michalsky <marcus.michalsky@ptb.de>
2016-05-11 16:46:14 +0200

    fixed sonar minor severity "Magic Number"

--- applications/plugins/org.csstudio.eve.viewer/src/de/ptb/epics/eve/viewer/views/plotview/plot/SampleSizeUpdateStrategy.java
+++ applications/plugins/org.csstudio.eve.viewer/src/de/ptb/epics/eve/viewer/views/plotview/plot/SampleSizeUpdateStrategy.java
@@ -2,11 +2,26 @@ package de.ptb.epics.eve.viewer.views.plotview.plot;
 
 
 /**
+ * SampleSizeUpdateStrategy decides whether to update upon the number of 
+ * samples. A larger sample size results in a less frequent update to save 
+ * resources.
+ * 
  * @author Marcus Michalsky
  * @since 1.13
  */
 public class SampleSizeUpdateStrategy implements UpdateStrategy {
-
+	private static final int SAMPLE_SIZE_THRESHOLD_1 = 100;
+	private static final int SAMPLE_SIZE_THRESHOLD_2 = 500;
+	private static final int SAMPLE_SIZE_THRESHOLD_3 = 2500;
+	private static final int SAMPLE_SIZE_THRESHOLD_4 = 5000;
+	private static final int SAMPLE_SIZE_THRESHOLD_5 = 12500;
+	
+	private static final int SAMPLE_COUNT_1 = 5;
+	private static final int SAMPLE_COUNT_2 = 10;
+	private static final int SAMPLE_COUNT_3 = 50;
+	private static final int SAMPLE_COUNT_4 = 100;
+	private static final int SAMPLE_COUNT_5 = 250;
+	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -20,16 +35,16 @@ public class SampleSizeUpdateStrategy implements UpdateStrategy {
 		 * /----- ab 5000 Punkten werden je 100 Punkte geplottet /
 		 * /----- ab 12500 Punkten werden je 250 Punkte geplottet /
 		 */
-		if (sampleSizeOfLastUpdate >= 12500) {
-			return (sampleSize -12500) % 250 == 0;
-		} else if (sampleSizeOfLastUpdate >= 5000) {
-			return (sampleSize -5000) % 100 == 0;
-		} else if (sampleSizeOfLastUpdate >= 2500) {
-			return (sampleSize -2500) % 50 == 0;
-		} else if (sampleSizeOfLastUpdate >= 500) {
-			return (sampleSize -500) % 10 == 0;
-		} else if (sampleSizeOfLastUpdate >= 100) {
-			return (sampleSize -100) % 5 == 0;
+		if (sampleSizeOfLastUpdate >= SAMPLE_SIZE_THRESHOLD_5) {
+			return (sampleSize - SAMPLE_SIZE_THRESHOLD_5) % SAMPLE_COUNT_5 == 0;
+		} else if (sampleSizeOfLastUpdate >= SAMPLE_SIZE_THRESHOLD_4) {
+			return (sampleSize - SAMPLE_SIZE_THRESHOLD_4) % SAMPLE_COUNT_4 == 0;
+		} else if (sampleSizeOfLastUpdate >= SAMPLE_SIZE_THRESHOLD_3) {
+			return (sampleSize - SAMPLE_SIZE_THRESHOLD_3) % SAMPLE_COUNT_3 == 0;
+		} else if (sampleSizeOfLastUpdate >= SAMPLE_SIZE_THRESHOLD_2) {
+			return (sampleSize - SAMPLE_SIZE_THRESHOLD_2) % SAMPLE_COUNT_2 == 0;
+		} else if (sampleSizeOfLastUpdate >= SAMPLE_SIZE_THRESHOLD_1) {
+			return (sampleSize - SAMPLE_SIZE_THRESHOLD_1) % SAMPLE_COUNT_1 == 0;
 		}
 		return true;
 	}
