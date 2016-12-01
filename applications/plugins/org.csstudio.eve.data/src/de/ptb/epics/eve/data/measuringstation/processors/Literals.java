ac9ae85ff8f3ddd247c7686238f37e797d95c067
Marcus Michalsky <marcus.michalsky@ptb.de>
2016-12-01 18:06:51 +0100

    fixed sonar major severity "Avoid Duplicate Literals"

---
+++ applications/plugins/org.csstudio.eve.data/src/de/ptb/epics/eve/data/measuringstation/processors/Literals.java
@@ -0,0 +1,21 @@
+package de.ptb.epics.eve.data.measuringstation.processors;
+
+final class Literals {
+	public static final String XML_ATTRIBUTE_NAME_COUNT = "count";
+	public static final String XML_ATTRIBUTE_NAME_MONITOR = "monitor";
+	public static final String XML_ATTRIBUTE_NAME_NAME = "name";
+	public static final String XML_ATTRIBUTE_NAME_TIMEOUT = "timeout";
+	public static final String XML_ATTRIBUTE_NAME_TRANSPORT = "transport";
+	public static final String XML_ATTRIBUTE_NAME_TYPE = "type";
+	
+	public static final String XML_ELEMENT_NAME_ACCESS = "access";
+	public static final String XML_ELEMENT_NAME_CLASS = "class";
+	public static final String XML_ELEMENT_NAME_DISPLAYGROUP = "displaygroup";
+	public static final String XML_ELEMENT_NAME_NAME = "name";
+	public static final String XML_ELEMENT_NAME_OPTION = "option";
+	public static final String XML_ELEMENT_NAME_UNIT = "unit";
+	public static final String XML_ELEMENT_NAME_STATUS = "status";
+	public static final String XML_ELEMENT_NAME_STOP = "stop";
+	public static final String XML_ELEMENT_NAME_TRIGGER = "trigger";
+	public static final String XML_ELEMENT_NAME_VALUE = "value";
+}
