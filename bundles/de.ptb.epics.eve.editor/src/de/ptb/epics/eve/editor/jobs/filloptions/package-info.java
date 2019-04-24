860cdb708081213d90e3c92d8e80fa96173ae150
Marcus Michalsky <marcus.michalsky@ptb.de>
2019-04-24 17:26:42 +0200

    feature #4260: removed obsolete fill options
    - removed command definitions
    - removed menu contribution
    - removed default handler
    - removed jobs

--- bundles/de.ptb.epics.eve.editor/src/de/ptb/epics/eve/editor/jobs/filloptions/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains {@link org.eclipse.core.runtime.jobs.Job}s related to fill options 
- * of a {@link de.ptb.epics.eve.data.scandescription.ScanModule}.
- * 
- * @author Marcus Michalsky
- * @since 1.1
- */
-package de.ptb.epics.eve.editor.jobs.filloptions;
