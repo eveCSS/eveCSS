fff97573a878edd354eb067e16263e56ef1dae38
Marcus Michalsky <marcus.michalsky@ptb.de>
2019-08-29 12:26:19 +0200

    implemented feature #4862

--- bundles/de.ptb.epics.eve.editor/src/de/ptb/epics/eve/editor/views/scanmoduleview/classiccomposite/motoraxiscomposite/LabelProvider.java
+++ bundles/de.ptb.epics.eve.editor/src/de/ptb/epics/eve/editor/views/scanmoduleview/classiccomposite/motoraxiscomposite/LabelProvider.java
@@ -1,8 +1,8 @@
 package de.ptb.epics.eve.editor.views.scanmoduleview.classiccomposite.motoraxiscomposite;
+
 import org.eclipse.swt.graphics.Image;
 
 import de.ptb.epics.eve.data.scandescription.Axis;
-import de.ptb.epics.eve.data.scandescription.Stepfunctions;
 import de.ptb.epics.eve.data.scandescription.errors.AxisError;
 import de.ptb.epics.eve.data.scandescription.errors.IModelError;
 import de.ptb.epics.eve.editor.views.scanmoduleview.classiccomposite.ActionCompositeLabelProvider;
@@ -40,23 +40,44 @@ public class LabelProvider extends ActionCompositeLabelProvider {
 	 */
 	@Override
 	public String getColumnText(final Object axis, final int colIndex) {
+		Axis smAxis = (Axis) axis;
 		switch (colIndex) {
-		case 1:
+		case 1: // name column
+			String stepfunction = ((Axis)axis).getStepfunction().toString();
 			if (((Axis)axis).isMainAxis()) {
-				return ((Axis)axis).getAbstractDevice().getName() + " " + (char) 171 + " Main Axis";
+				return ((Axis)axis).getAbstractDevice().getName() + " (" + stepfunction + ") " + (char) 171 + " Main Axis";
 			}
-			return ((Axis)axis).getAbstractDevice().getName();
-		case 2:
-			if (Stepfunctions.PLUGIN.equals(((Axis)axis).getStepfunction())) {
+			return ((Axis)axis).getAbstractDevice().getName() + " (" + stepfunction + ")";
+		case 2: // value column
+			switch (((Axis)axis).getStepfunction()) {
+			case ADD:
+				return smAxis.getStart().toString() + '\u2192' +
+						smAxis.getStop().toString() + " / " + 
+						smAxis.getStepwidth().toString();
+			case FILE:
+				if (smAxis.getFile() != null && smAxis.getFile().getName() != null) {
+					return ((Axis)axis).getFile().getName();
+				}
+				return "<path invalid>";
+			case MULTIPLY:
+				return smAxis.getStart().toString() + '\u2192' +
+						smAxis.getStop().toString() + " / " + 
+						smAxis.getStepwidth().toString();
+			case PLUGIN:
 				if (((Axis)axis).getPluginController() == null || 
 					((Axis)axis).getPluginController().getPlugin() == null) {
 						return "Plugin";
 				}
 				return "Plugin (" + ((Axis)axis).getPluginController().
-						getPlugin().getName() + ")";
+					getPlugin().getName() + ")";
+			case POSITIONLIST:
+				return ((Axis)axis).getPositionlist();
+			case RANGE:
+				return ((Axis)axis).getRange();
+			default:
+				return "--";
 			}
-			return ((Axis)axis).getStepfunction().toString();
-		case 3:
+		case 3: // #points column
 			return ((Axis)axis).getMode().getPositionCount() == null 
 				? "N/A"
 				: ((Axis)axis).getMode().getPositionCount().toString();
