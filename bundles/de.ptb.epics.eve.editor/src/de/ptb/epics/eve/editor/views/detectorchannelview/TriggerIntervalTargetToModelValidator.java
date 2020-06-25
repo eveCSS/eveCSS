c9a5355406e837a3cb7ce3efb40de53f8b3f05a3
Marcus Michalsky <marcus.michalsky@ptb.de>
2020-06-25 18:59:46 +0200

    #5225: validation and feedback for (empty) trigger interval input

---
+++ bundles/de.ptb.epics.eve.editor/src/de/ptb/epics/eve/editor/views/detectorchannelview/TriggerIntervalTargetToModelValidator.java
@@ -0,0 +1,35 @@
+package de.ptb.epics.eve.editor.views.detectorchannelview;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.runtime.IStatus;
+
+import com.ibm.icu.text.NumberFormat;
+
+/**
+ * @author Marcus Michalsky
+ * @since 1.34
+ */
+public class TriggerIntervalTargetToModelValidator implements IValidator {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public IStatus validate(Object value) {
+		String val = (String)value;
+		if (val.isEmpty()) {
+			return ValidationStatus.error("Trigger Interval is mandatory!");
+		}
+		try {
+			NumberFormat.getInstance(Locale.US).parse(val);
+		} catch (ParseException e) {
+			return ValidationStatus.error(e.getMessage());
+		}
+		return ValidationStatus.ok();
+	}
+
+}
