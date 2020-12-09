e95842a410beeb14f363a17fbbc4857df6cf41e2
Marcus Michalsky <marcus.michalsky@ptb.de>
2020-12-09 19:48:06 +0100

    #5739: fixed main axis stepcount adjustment + unit/integration tests

--- tests/de.ptb.epics.eve.data.tests/src/de/ptb/epics/eve/data/scandescription/tests/ScanModuleTest.java
+++ tests/de.ptb.epics.eve.data.tests/src/de/ptb/epics/eve/data/scandescription/tests/ScanModuleTest.java
@@ -4,13 +4,8 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -21,9 +16,6 @@ import de.ptb.epics.eve.data.scandescription.Axis;
 import de.ptb.epics.eve.data.scandescription.Chain;
 import de.ptb.epics.eve.data.scandescription.Channel;
 import de.ptb.epics.eve.data.scandescription.ScanModule;
-import de.ptb.epics.eve.data.scandescription.Stepfunctions;
-import de.ptb.epics.eve.data.scandescription.axismode.AddMultiplyMode;
-import de.ptb.epics.eve.data.scandescription.axismode.AdjustParameter;
 import de.ptb.epics.eve.data.scandescription.channelmode.ChannelModes;
 import de.ptb.epics.eve.data.scandescription.updatenotification.IModelUpdateListener;
 import de.ptb.epics.eve.data.scandescription.updatenotification.ModelUpdateEvent;
@@ -39,11 +31,8 @@ import de.ptb.epics.eve.data.tests.mothers.scandescription.ScanModuleMother;
  * @author Marcus Michalsky
  * @since 1.23
  */
-public class ScanModuleTest implements IModelUpdateListener, PropertyChangeListener {
+public class ScanModuleTest implements IModelUpdateListener {
 	private ScanModule scanModule;
-	private Axis axis1;
-	private Axis axis2;
-	private boolean mainAxis;
 	
 	private boolean modelUpdate;
 	
@@ -201,191 +190,10 @@ public class ScanModuleTest implements IModelUpdateListener, PropertyChangeListe
 	public void updateEvent(ModelUpdateEvent modelUpdateEvent) {
 		this.modelUpdate = true;
 	}
-
-	// TODO older tests should be moved to e.g. ScanModuleClassicTest
-	// and one Test File for each scanmodule type should be created
-	
-	/**
-	 * Inserts two axes into a scan module, sets one as main axes and validates 
-	 * that the step width is adjusted.
-	 * 
-	 * @since 1.23
-	 */
-	@Test
-	public void testSetMainAxis() {
-		this.mockAxes();
-		
-		axis1.setStepfunction(Stepfunctions.ADD);
-		axis2.setStepfunction(Stepfunctions.ADD);
-		
-		axis1.setStart(0.0);
-		axis1.setStop(100.0);
-		axis1.setStepwidth(10.0);
-		
-		axis2.setStart(0.0);
-		axis2.setStop(20.0);
-		axis2.setStepwidth(1.0); // should be 2 after setting main axis
-		
-		this.scanModule.add(axis1);
-		this.scanModule.add(axis2);
-		
-		axis1.setMainAxis(true);
-		
-		assertEquals(2.0, (double) axis2.getStepwidth(), 0);
-	}
-	
-	/**
-	 * Inserts an axis into a scan module, sets it as main axis and adds another 
-	 * axis. The second axis' stepwidth should be adjusted.
-	 * 
-	 * @since 1.23
-	 */
-	@Test
-	public void testAddAxisAfterSetMainAxis() {
-		this.mockAxes();
-		
-		axis1.setStepfunction(Stepfunctions.ADD);
-		axis2.setStepfunction(Stepfunctions.ADD);
-		
-		axis1.setStart(0.0);
-		axis1.setStop(100.0);
-		axis1.setStepwidth(10.0);
-		
-		this.scanModule.add(axis1);
-		axis1.setMainAxis(true);
-		
-		axis2.setStart(0.0);
-		axis2.setStop(20.0);
-		axis2.setStepwidth(1.0); // should be 2 after setting main axis
-		
-		this.scanModule.add(axis2);
-		
-		assertEquals(2.0, (double) axis2.getStepwidth(), 0);
-	}
-	
-	/**
-	 * Adds an axis to a scan module and another one with the MainAxis Property
-	 * enabled. The first axis' stepwidth should be adjusted.
-	 * 
-	 * @since 1.23
-	 */
-	@Test
-	public void testAddMainAxis() {
-		this.mockAxes();
-		
-		axis1.setStepfunction(Stepfunctions.ADD);
-		axis2.setStepfunction(Stepfunctions.ADD);
-		
-		axis2.setStart(0.0);
-		axis2.setStop(20.0);
-		axis2.setStepwidth(1.0); // should be 2 after adding main axis
-		
-		this.scanModule.add(axis2);
-		
-		axis1.setStart(0.0);
-		axis1.setStop(100.0);
-		axis1.setStepwidth(10.0);
-		
-		axis1.setMainAxis(true);
-		this.scanModule.add(axis1);
-		
-		assertEquals(2.0, (double) axis2.getStepwidth(), 0);
-	}
-	
-	/**
-	 * Set an Axis as main axis when another one is already set. The old one 
-	 * should be replaced.
-	 * 
-	 * @since 1.24
-	 */
-	@Test
-	public void testOverwriteMainAxis() {
-		Axis axis1 = mockAxis();
-		Axis axis2 = mockAxis();
-		Axis axis3 = mockAxis();
-		
-		axis1.setStepfunction(Stepfunctions.ADD);
-		axis2.setStepfunction(Stepfunctions.ADD);
-		axis3.setStepfunction(Stepfunctions.ADD);
-		
-		axis1.setStart(0.0);
-		axis1.setStop(10.0);
-		axis1.setStepwidth(1.0);
-		
-		axis2.setStart(0.0);
-		axis2.setStop(10.0);
-		axis2.setStepwidth(2.0);
-		
-		axis3.setStart(0.0);
-		axis3.setStop(10.0);
-		axis3.setStepwidth(5.0);
-		
-		this.scanModule.add(axis1);
-		this.scanModule.add(axis2);
-		this.scanModule.add(axis3);
-		
-		axis1.setMainAxis(true);
-		
-		assertEquals(1.0, (double) axis2.getStepwidth(), 0);
-		assertEquals(1.0, (double) axis3.getStepwidth(), 0);
-		
-		axis3.setMainAxis(true);
-		
-		assertTrue(axis3.isMainAxis());
-		assertEquals(AdjustParameter.STEPCOUNT, 
-				((AddMultiplyMode<?>)axis3.getMode()).getAdjustParameter());
-		
-		assertFalse(axis1.isMainAxis());
-		assertEquals(AdjustParameter.STEPWIDTH, 
-				((AddMultiplyMode<?>)axis1.getMode()).getAdjustParameter());
-		
-		axis3.setStepwidth(5.0);
-		
-		assertEquals(5.0, (double) axis1.getStepwidth(), 0);
-		assertEquals(5.0, (double) axis2.getStepwidth(), 0);
-	}
-	
-	/**
-	 * Adds two axes to a scan module, sets one as main axis and removes it.
-	 * The remaining axis should be editable. The ScanModule should not have 
-	 * a main axis anymore.
-	 * 
-	 * @since 1.23
-	 */
-	@Test
-	public void testRemoveMainAxis() {
-		this.mockAxes();
-		
-		axis1.setStart(0.0);
-		axis1.setStop(100.0);
-		axis1.setStepwidth(10.0);
-		
-		axis2.setStart(0.0);
-		axis2.setStop(20.0);
-		axis2.setStepwidth(1.0); 
-		
-		this.scanModule.add(axis1);
-		this.scanModule.add(axis2);
-		
-		this.mainAxis = false;
-		
-		assertNull(this.scanModule.getMainAxis());
-		assertFalse(this.mainAxis);
-		
-		this.scanModule.addPropertyChangeListener(ScanModule.MAIN_AXIS_PROP, this);
-		axis1.setMainAxis(true);
-		assertTrue(this.mainAxis);
-		assertEquals(axis1, this.scanModule.getMainAxis());
-		
-		this.scanModule.remove(axis1);
-		
-		assertFalse(this.mainAxis);
-		assertNull(this.scanModule.getMainAxis());
-	}
 	
 	@Test
 	public void testIsUsedAsNormalizeChannel() {
-		ScanModule scanModule = ScanModuleMother.createNewScanModule();
+		this.scanModule = ScanModuleMother.createNewScanModule();
 		Channel channel1 = ChannelMother.createNewChannel(scanModule);
 		scanModule.add(channel1);
 		try {
@@ -407,7 +215,7 @@ public class ScanModuleTest implements IModelUpdateListener, PropertyChangeListe
 
 	@Test
 	public void testIsUsedAsStoppedByChannel() {
-		ScanModule scanModule = ScanModuleMother.createNewScanModule();
+		this.scanModule = ScanModuleMother.createNewScanModule();
 		Channel channel1 = ChannelMother.createNewChannel(scanModule);
 		scanModule.add(channel1);
 		try {
@@ -427,19 +235,6 @@ public class ScanModuleTest implements IModelUpdateListener, PropertyChangeListe
 		channel1.setStoppedBy(null);
 		assertFalse(scanModule.isUsedAsStoppedByChannel(channel2));
 	}
-
-	@Override
-	public void propertyChange(PropertyChangeEvent evt) {
-		switch (evt.getPropertyName()) {
-		case ScanModule.MAIN_AXIS_PROP:
-			if (evt.getNewValue() != null) {
-				this.mainAxis = true;
-			} else {
-				this.mainAxis = false;
-			}
-			break;
-		}
-	}
 	
 	/*
 	 * Creates a Mock Axis
@@ -457,51 +252,14 @@ public class ScanModuleTest implements IModelUpdateListener, PropertyChangeListe
 		return axis;
 	}
 	
-	/*
-	 * @Deprecated - Should not be Used, use mockAxis():Axis instead
-	 */
-	private void mockAxes() {
-		axis1 = new Axis(this.scanModule);
-		axis2 = new Axis(this.scanModule);
-		MotorAxis mockAxis = mock(MotorAxis.class);
-		when(mockAxis.getType()).thenReturn(DataTypes.DOUBLE);
-		when(mockAxis.getDefaultValue()).thenReturn("0");
-		when(mockAxis.getName()).thenReturn("MockAxis");
-		Function mockFunction = mock(Function.class);
-		when(mockFunction.isDiscrete()).thenReturn(false);
-		when(mockAxis.getGoto()).thenReturn(mockFunction);
-		axis1.setMotorAxis(mockAxis);
-		axis2.setMotorAxis(mockAxis);
-	}
-	
-	/**
-	 * Class wide setup method of the test
-	 */
-	@BeforeClass
-	public static void runBeforeClass() {
-	}
-	
-	/**
-	 * test wide set up method
-	 */
 	@Before
-	public void beforeEveryTest() {
+	public void beforeTest() {
 		this.scanModule = new ScanModule(1);
 		this.scanModule.addModelUpdateListener(this);
 	}
 	
-	/**
-	 * test wide tear down method
-	 */
 	@After
-	public void afterEveryTest() {
+	public void afterTest() {
 		this.scanModule = null;
-	}
-	
-	/**
-	 * class wide tear down method
-	 */
-	@AfterClass
-	public static void afterClass() {
 	}
 }
